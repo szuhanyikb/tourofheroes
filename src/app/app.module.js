f7624bd563c8fdc47b4473dab268662224e1ae6b
Bence Szuhanyik <szuhanyikb@gmail.com>
2017-09-05 14:39:32 +0200

    Removed comment

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -7,7 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
-var forms_1 = require("@angular/forms"); // <-- NgModel lives here
+var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
 var hero_detail_component_1 = require("./hero-detail.component");
 var AppModule = (function () {
